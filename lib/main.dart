73a7ad07ee6cc01147caceb1c262a8cf923214ea
asmikhare <asmikhareofficial@gmail.com>
2024-11-25 01:51:44 +0530

    vpn

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,13 @@ import 'package:gailtrack/app/index.dart';
 import 'package:gailtrack/app/providers.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:gailtrack/firebase_options.dart';
+import 'package:logging/logging.dart';
 
 void main() async {
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen((record) {
+    print('${record.level.name}: ${record.time}: ${record.message}');
+  });
   //* Ensures Firebase is Initialised Properly
   WidgetsFlutterBinding.ensureInitialized();
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
 import firebase_auth
 import firebase_core
 import flutter_secure_storage_macos
@@ -12,6 +13,7 @@ import local_auth_darwin
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
