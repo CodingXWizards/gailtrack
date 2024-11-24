2cbf5b6872281686833f11d84d1f798ff0c86f73
yashsinghcodes <yash9vardhan@gmail.com>
2024-11-25 01:26:50 +0530

    [fix]: added check for biometric before onboarding page

--- lib/utils/helper.dart
+++ lib/utils/helper.dart
@@ -1,5 +1,14 @@
 import 'package:local_auth/local_auth.dart';
 
+Future<bool> checkBiometric() async {
+  final LocalAuthentication localAuth = LocalAuthentication();
+
+  final bool canAuthenticateWithBiometrics = await localAuth.canCheckBiometrics;
+  final bool canAuthenticate = canAuthenticateWithBiometrics && await localAuth.isDeviceSupported();
+  
+  return canAuthenticate;
+}
+
 Future<bool> authenticateWithBiometrics() async {
   final LocalAuthentication localAuth = LocalAuthentication();
 
