1959d2ffba3ba1155b9ebabac005445ea1cab18c
Raja Singh <rajasgh18@gmail.com>
2024-12-03 02:41:39 +0530

    feat: added tasks and settings screen

--- lib/screens/home/tabs/home/index.dart
+++ lib/screens/home/tabs/home/index.dart
@@ -12,87 +12,82 @@ class HomeTab extends StatefulWidget {
 class _HomeTabState extends State<HomeTab> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Row(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: BoxContainer(
+                child: Column(
+                  children: [
+                    Text(
+                      "Checked-in",
+                      style: Theme.of(context).textTheme.titleSmall,
+                    ),
+                    Text("8:45am",
+                        style: Theme.of(context).textTheme.titleMedium),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(width: 12),
+            Expanded(
+              child: BoxContainer(
+                child: Column(
+                  children: [
+                    Text(
+                      "Checked-out",
+                      style: Theme.of(context).textTheme.titleSmall,
+                    ),
+                    Text("------",
+                        style: Theme.of(context).textTheme.titleMedium),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(height: 12),
+        BoxContainer(
+          child: Column(
             children: [
-              Expanded(
-                child: BoxContainer(
-                  child: Column(
-                    children: [
-                      Text(
-                        "Checked-in",
-                        style: Theme.of(context).textTheme.titleSmall,
-                      ),
-                      Text("8:45am",
-                          style: Theme.of(context).textTheme.titleMedium),
-                    ],
-                  ),
-                ),
+              Text(
+                "Working Hours",
+                style: Theme.of(context).textTheme.titleSmall,
               ),
-              const SizedBox(width: 12),
-              Expanded(
-                child: BoxContainer(
-                  child: Column(
-                    children: [
-                      Text(
-                        "Checked-out",
-                        style: Theme.of(context).textTheme.titleSmall,
-                      ),
-                      Text("------",
-                          style: Theme.of(context).textTheme.titleMedium),
-                    ],
-                  ),
-                ),
-              ),
+              Text("9 hours 7 minutes",
+                  style: Theme.of(context).textTheme.titleMedium),
             ],
           ),
-          const SizedBox(height: 12),
-          BoxContainer(
-            child: Column(
-              children: [
-                Text(
-                  "Working Hours",
-                  style: Theme.of(context).textTheme.titleSmall,
-                ),
-                Text("9 hours 7 minutes",
-                    style: Theme.of(context).textTheme.titleMedium),
-              ],
-            ),
-          ),
-          const SizedBox(height: 32),
-          CustomCard(
-            title: "Offisite Work",
-            description:
-                "You can request to work offsite if you're unable to come in.",
-            buttonText: "Request Offsite Work",
-            imagePath: "assets/images/bg1.png",
-            onPressed: () =>
-                Navigator.of(context).pushNamed('/request/offsite'),
-          ),
-          const SizedBox(height: 32),
-          CustomCard(
-            title: "Request a leave or HalfDay",
-            description:
-                "You can request a leave from a work to your superiors.",
-            buttonText: "Request a leave",
-            imagePath: "assets/images/bg1.png",
-            onPressed: () => Navigator.pushNamed(context, '/request/leave'),
-          ),
-          const SizedBox(height: 32),
-          ElevatedButton(
-              style: const ButtonStyle(
-                  backgroundColor: WidgetStatePropertyAll<Color>(Colors.white)),
-              onPressed: () {},
-              child: Text(
-                "Leaderboard",
-                style: Theme.of(context).textTheme.labelSmall,
-              ))
-        ],
-      ),
+        ),
+        const SizedBox(height: 32),
+        CustomCard(
+          title: "Offisite Work",
+          description:
+              "You can request to work offsite if you're unable to come in.",
+          buttonText: "Request Offsite Work",
+          imagePath: "assets/images/bg1.png",
+          onPressed: () => Navigator.of(context).pushNamed('/request/offsite'),
+        ),
+        const SizedBox(height: 32),
+        CustomCard(
+          title: "Request a leave or HalfDay",
+          description: "You can request a leave from a work to your superiors.",
+          buttonText: "Request a leave",
+          imagePath: "assets/images/bg1.png",
+          onPressed: () => Navigator.pushNamed(context, '/request/leave'),
+        ),
+        const SizedBox(height: 32),
+        ElevatedButton(
+            style: const ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll<Color>(Colors.white)),
+            onPressed: () {},
+            child: Text(
+              "Leaderboard",
+              style: Theme.of(context).textTheme.labelSmall,
+            ))
+      ],
     );
   }
 }
